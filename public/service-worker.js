249340c754be3666d1c01294aeb524c521213613
gdickey273 <gdickey273@gmail.com>
2020-10-29 11:22:27 -0400

    changed the static-cache version many times

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,6 @@
 console.log("------------------its workinggggggg!------------------ \n ------------------------------------------- \n ----------------------");
 
-const CACHE_NAME = "static-cache-v2";
+const CACHE_NAME = "static-cache-v7";
 const DATA_CACHE_NAME = "data-cache-v1";
 const FILES_TO_CACHE = [
   "/",
